f324837f7b88c226b58ad26c46bb3bc346865efd
PriviMW <privimw88@gmail.com>
2026-05-13 15:22:58 +0800

    bump: v1.6.2 (versionCode 14)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.privimemobile"
         minSdk = 24
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.6.1"
+        versionCode = 14
+        versionName = "1.6.2"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
